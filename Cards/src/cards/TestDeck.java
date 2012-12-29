290100104b344f08ca220f38f555d9fbdb96aa17
Shail <shetyeshail@gmail.com>
2012-12-29 12:27:09 -0500

    V1.0
    as is, right out of the book

---
+++ Cards/src/cards/TestDeck.java
@@ -0,0 +1,14 @@
+package cards;
+public class TestDeck{
+    
+    public static void main(String[] args){
+        Deck deck = new Deck();
+        deck.shuffle();
+        int count = 0;
+        while (! deck.isEmpty()){
+            count++;
+            Card card = deck.deal();
+            System.out.println(count + ": " + card);
+        }
+    }
+}
